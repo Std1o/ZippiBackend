8e5501a2158741a4df64bd3cc56cf74267650377
Std1o <37378410+Std1o@users.noreply.github.com>
2026-05-17 20:34:08 +0300

    fix

--- zippi/api/images.py
+++ zippi/api/images.py
@@ -9,12 +9,12 @@ router = APIRouter(prefix='/images', tags=["images"])
 
 @router.get('/{image_name}')
 def get_image(image_name: str):
-    return FileResponse(f"taximetr/images/{image_name}".encode('utf-8').decode('utf-8'))
+    return FileResponse(f"zippi/images/{image_name}".encode('utf-8').decode('utf-8'))
 
 @router.post("/upload/")
 async def create_upload_file(request: Request, upload_file: UploadFile):
     try:
-        file_path = f"taximetr/images/{upload_file.filename}"
+        file_path = f"zippi/images/{upload_file.filename}"
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(upload_file.file, buffer)
     finally:
